d1bc3231e6f2545d3fc45bd7a357791babea1b15
Jeffrey Durieux <jeffreydurieux@users.noreply.github.com>
2026-02-02 16:15:43 +0100

    Finished knn part

--- Modeling.R
+++ Modeling.R
@@ -6,3 +6,52 @@
 # Knn
 # Randomforest
 # XgBoost
+
+load('data.Rdata')
+attach(data)
+#### Train test split #####
+
+cut_date <- as.Date("2025-10-01")
+
+train <- data[data$date < cut_date, ]
+test  <- data[data$date >= cut_date, ]
+
+
+##### Poisson #####
+
+fit_poisson <- glm(deliveries ~ rush + t + weekday +
+             seasonality1 + seasonality2 + val_window + mom_window + xmas_window,
+           family = poisson(), data = train)
+
+
+##### KNN #####
+
+library(caret)
+
+train$weekday <- factor(train$weekday)
+test$weekday <- factor(test$weekday)
+
+ctrl <- trainControl(
+  method = "cv",
+  number = 10
+)
+
+set.seed(42)
+
+knn_fit <- train(
+  deliveries ~ t + rush + weekday + seasonality1 + seasonality2 +
+    val_window + mom_window + xmas_window,
+  data = train,
+  method = "knn",
+  trControl = ctrl,
+  preProcess = c("center", "scale"),
+  tuneGrid = data.frame(k = seq(1,30, by = 2) )
+)
+
+knn_fit
+
+knn_pred <- predict(knn_fit, test)
+
+rmse_knn <- sqrt(mean((test$deliveries - knn_pred)^2))
+mae_knn  <- mean(abs(test$deliveries - knn_pred))
+rmse_knn;mae_knn
